d8ab047cf7d01e24a4c65adf0223796241e5bdbf
axelw <wolfs.axelw@gmail.com>
2022-11-02 20:44:43 +0100

    before add twig/intl extra

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Voitures;
 use App\Repository\VoituresRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -18,10 +19,18 @@ class HomeController extends AbstractController
     }
 
     #[Route('/voitures', name: 'voiture')]
-    public function voiture(VoituresRepository $voitures): Response
+    public function voitures(VoituresRepository $voitures): Response
     {
         return $this->render('voitures.html.twig', [
             'voitures' => $voitures->findAll(),
+        ]);
+    }
+
+    #[Route('/voiture/{slug}', name: 'voiture_show')]
+    public function voitureShow(Voitures $voitures): Response
+    {
+        return $this->render('voitureShow.html.twig', [
+            'voiture' => $voitures
         ]);
     }
 }
