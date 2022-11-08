bf7e23cc6f0c36ab1a2e86e555fba9b835a810af
axelw <wolfs.axelw@gmail.com>
2022-11-08 17:47:14 +0100

    ajout new car style

--- src/Form/VoituresType.php
+++ src/Form/VoituresType.php
@@ -2,33 +2,72 @@
 
 namespace App\Form;
 
+use App\Entity\Marques;
 use App\Entity\Voitures;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\FileType;
+use FOS\CKEditorBundle\Form\Type\CKEditorType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class VoituresType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('modelName')
-            ->add('prix')
-            ->add('kilometrage')
-            ->add('nbr_propi')
-            ->add('puissance')
-            ->add('cylindre')
-            ->add('description')
-            ->add('Carburant')
-            ->add('date')
-            ->add('slug')
-            ->add('marques')
+            ->add('modelName', TextType::class,[
+                'attr' => ['class' => 'form-control'],
+            ]
+            )
+            ->add('prix', IntegerType::class, [
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('kilometrage', IntegerType::class,[
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('nbr_propi', IntegerType::class, [
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('puissance', IntegerType::class, [
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('cylindre', IntegerType::class, [
+                'attr' => ['class' => 'form-control'],
+            ])
+
+            ->add('description', CKEditorType::class, array(
+                'config' => array(
+                    'uiColor' => '#ffffff',
+                ),
+            ))
+            ->add('carburant', TextType::class,[
+                'attr' => ['class' => 'form-control'],
+            ])
+            ->add('date', DateType::class, [
+                'attr' => ['class' => 'form-control'],
+                'widget' => 'single_text'
+            ])
+            ->add('slug', TextType::class, [
+                'attr' => ['class' => 'form-control'],
+            ])
+
+            ->add('marques', ChoiceType::class, [
+                // 'attr' => ['class' => 'form-control'],
+                'choice_attr' => function (?Marques $marques) {
+                    return $marques ? ['class' => 'category_'.strtolower($marques->getNom())] : [];
+                },
+            ])
+
             ->add('images', FileType::class, [
                 'label' => false,
                 'multiple' => true,
                 'mapped' => false,
-                'required' => false
+                'required' => false,
+                'attr' => ['class' => 'form-control mt-3'],
             ])
         ;
     }
